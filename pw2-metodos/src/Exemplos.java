165dd4ec9317eb63dc624f6eceeb51acfa3b9b38
Marco Royo Felippe <maroyofelippe@gmail.com>
2020-03-04 20:02:32 -0300

    Revisão de IF

---
+++ pw2-metodos/src/Exemplos.java
@@ -0,0 +1,14 @@
+
+public class Exemplos {
+	
+	public static void main(String[] args) {
+		int idade = 17;
+		
+		if (idade >= 18) {
+			System.out.println("Maior de idade");
+		}else {
+			System.out.println("Menor de idade");
+		}
+	}
+
+}
