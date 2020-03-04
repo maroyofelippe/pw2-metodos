114d01e0715e67949f7afc4eda875e924e40318f
Marco Royo Felippe <maroyofelippe@gmail.com>
2020-03-04 20:36:29 -0300

    Exemplos de Metodos

--- pw2-metodos/src/Exemplos.java
+++ pw2-metodos/src/Exemplos.java
@@ -2,13 +2,40 @@
 public class Exemplos {
 	
 	public static void main(String[] args) {
-		int idade = 17;
 		
+		ehMaiorIdade(9);
+		
+		ehMaiorIdade(30);
+		
+		classificar(0);
+		}	
+	
+	
+	public static void ehMaiorIdade(int idade) {
 		if (idade >= 18) {
-			System.out.println("Maior de idade");
+			System.out.println("Maior de Idade");
 		}else {
-			System.out.println("Menor de idade");
+			System.out.println("Menor de Idade");
 		}
 	}
 
+	public static void classificar(int num) {
+		
+		if (num > 0) {
+			System.out.println("Positivo");
+		}else if (num < 0) {
+			System.out.println("Negativo");
+		}else {
+			System.out.println("Zero");
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
